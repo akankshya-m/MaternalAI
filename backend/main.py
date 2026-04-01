5d82362ea4523cdcc008e14f420007c8ca9511ff
Akankshya Mahapatra <akankshyamahapatra96@gmail.com>
2026-04-01 20:15:34 +0530

    running main.py launches server

--- backend/main.py
+++ backend/main.py
@@ -88,3 +88,8 @@ def fetch_feedback():
         return get_all_feedback()
     except Exception as e:
         raise HTTPException(status_code=500, detail=str(e))
+
+
+if __name__ == "__main__":
+    import uvicorn
+    uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True)
